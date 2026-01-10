e5fa29a3a8df3f3f949951c9e487531e559a72d8
Arturo Mendoza <Arturo.mendoza3@unmsm.edu.pe>
2026-01-10 17:56:21 -0500

    funcional 2

--- web/src/components/ServicesSection.tsx
+++ web/src/components/ServicesSection.tsx
@@ -7,9 +7,9 @@ const ServicesSection: React.FC = () => {
     <section className="py-24 bg-white" id="services">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* 🔴 TEST TAILWIND */}
-        <div className="text-5xl font-bold text-red-600 p-10 text-center">
+        {/*<div className="text-5xl font-bold text-red-600 p-10 text-center">
           Tailwind OK
-        </div>
+        </div>*/}
         <div className="text-center mb-16">
           <h2 className="text-sky-600 font-semibold tracking-wide uppercase text-sm mb-3">Nuestra Experticia</h2>
           <p className="text-3xl md:text-4xl font-bold text-slate-900">Servicios Cloud Especializados</p>
